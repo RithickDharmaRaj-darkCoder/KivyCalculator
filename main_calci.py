b7f021c733d70cecfeb6603ce7ef19d1f4dc9ee5
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2022-05-12 04:41:15 +0530

    Initial files are set for Kivy Calculator App...

---
+++ main_calci.py
@@ -0,0 +1,25 @@
+import kivy
+kivy.require('2.1.0')       # Minimum Version
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder       # To attach .kv file
+from kivy.core.window import Window
+
+# Attaching .kv file...
+Builder.load_file('calci.kv')
+
+# Window Size...
+Window.size = (370, 550)
+
+
+class CalciLayouts(Widget):
+    pass
+
+class CalculatorApp(App):
+    def build(self):
+        return CalciLayouts()
+
+
+if __name__ == '__main__':
+    CalculatorApp().run()
