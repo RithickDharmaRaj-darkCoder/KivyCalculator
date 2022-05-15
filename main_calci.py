efebc56a273e003a47ba1963ac1b86674ddb3324
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2022-05-15 06:18:08 +0530

    Number Calculations are done...

--- main_calci.py
+++ main_calci.py
@@ -14,9 +14,19 @@ Window.size = (370, 550)
 
 
 class CalciLayouts(Widget):
+    A = ''        # Operand 1...
+    Symbol = ''       # Last Operator...
+    B = ''       # Operand 2...
+    Current = 'A'       # To know where the number to insert(A/B)...
+
     # Clear function to clear everything from screen...
     def clear(self):
         self.ids.screen.text = '0'
+        self.ids.screen2.text = ''
+        self.A = ''  # Operand 1...
+        self.Symbol = ''  # Last Operator...
+        self.B = ''  # Operand 2...
+        self.Current = 'A'  # To know where the number to insert(A/B)...
 
     def backspace(self):
         pre_num = self.ids.screen.text
@@ -27,19 +37,60 @@ class CalciLayouts(Widget):
             self.ids.screen.text = '0'
 
     def num_btn_fn(self, num):
-        # Variable to store pre-entered values...
-        pre_num = self.ids.screen.text
+        try:
+            self.ids.screen2.font_size = 38
+            screen1_pre_value = self.ids.screen.text
+            screen2_pre_value = self.ids.screen2.text
 
-        # Checking is '0' in screen...
-        if pre_num == '0':
-            self.ids.screen.text = ''
-            self.ids.screen.text = str(num)
-        else:
-            self.ids.screen.text = f'{pre_num}{str(num)}'
+            if screen1_pre_value == '0' and (screen2_pre_value == '' or screen2_pre_value == 'Can\'t divide by 0' or screen2_pre_value == 'Can\'t start with (+,-,*,/)'):
+                self.A = str(num)
+                self.ids.screen.text = str(num)
+                self.ids.screen2.text = str(num)
+            else:
+                dupli_s1_pre_value = screen1_pre_value
+                s1_list = list(dupli_s1_pre_value)
+                last_in_list = s1_list[-1]
+                if ((last_in_list.isnumeric()) == True) and (self.Current == 'A'):
+                    self.A = self.A + str(num)
+                    self.ids.screen.text = f'{screen1_pre_value}{str(num)}'
+                    self.ids.screen2.text = str(eval(self.A))
+                elif ((last_in_list.isnumeric()) == False) and (self.Current == 'B'):
+                    self.B = str(num)
+                    answer = self.A + self.Symbol + self.B
+                    ans = eval(answer)
+                    self.ids.screen.text = f'{screen1_pre_value}{str(num)}'
+                    self.ids.screen2.text = str(ans)
+                elif ((last_in_list.isnumeric()) == True) and (self.Current == 'B'):
+                    self.B = self.B + str(num)
+                    answer = self.A + self.Symbol + self.B
+                    ans = eval(answer)
+                    self.ids.screen.text = f'{screen1_pre_value}{str(num)}'
+                    self.ids.screen2.text = str(ans)
+        except ZeroDivisionError:
+            self.ids.screen2.font_size = 18
+            self.ids.screen2.text = 'Can\'t divide by 0'
+            self.ids.screen.text = '0'
+            self.A = ''  # Operand 1...
+            self.Symbol = ''  # Last Operator...
+            self.B = ''  # Operand 2...
+            self.Current = 'A'  # To know where the number to insert(A/B)...
 
     def math_sign(self, sign):
-        pre_num = self.ids.screen.text
-        self.ids.screen.text = f'{pre_num}{sign}'
+        if self.ids.screen.text == '0':
+            self.ids.screen2.font_size = 18
+            self.ids.screen2.text = 'Can\'t start with (+,-,*,/)'
+            self.A = ''  # Operand 1...
+            self.Symbol = ''  # Last Operator...
+            self.B = ''  # Operand 2...
+            self.Current = 'A'  # To know where the number to insert(A/B)...
+        else:
+            self.A = self.ids.screen2.text
+            self.B = ''
+            self.Current = 'B'
+            self.Symbol = sign
+            pre_num = self.ids.screen.text
+            self.ids.screen.text = f'{pre_num}{sign}'
+
 
     def dot(self):
         pre_num = self.ids.screen.text
@@ -49,17 +100,20 @@ class CalciLayouts(Widget):
             pre_num = f'{pre_num}.'
             self.ids.screen.text = pre_num
 
-    def equal(self):
-        pre_num = self.ids.screen.text
+'''    def equal(self):
+        try:
+            pre_num = self.ids.screen.text
 
-        # Addition Stuff...
-        if '+' in pre_num:
-            num_list = pre_num.split('+')
-            ans = 0
-            for num in num_list:
-                ans += float(num)
-
-            self.ids.screen2.text = f'{str(ans)}  '
+            # Doing calculation using Math...
+            answer = eval(pre_num)
+            self.ids.screen2.text = str(answer)
+        except ZeroDivisionError:
+            self.ids.screen2.text = 'Can\'t divide by 0'
+            self.ids.screen.text = '0'
+        except:
+            self.ids.screen2.text = 'Error!'
+            self.ids.screen.text = '0'
+'''
 
 class CalculatorApp(App):
     def build(self):
